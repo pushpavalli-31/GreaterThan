1cd464b8bf6badbf7061a89a58ba9d82b6457958
Pushpavalli <illa.pushpavalli@capgemini.com>
2024-03-19 13:08:21 +0530

    GreaterThan Project

---
+++ src/com/greaterthan/GreaterThan.java
@@ -0,0 +1,13 @@
+package com.greaterthan;
+
+public class GreaterThan {
+
+	public static void main(String[] args) {
+			    int x = 5;
+			    int y = 3;
+			    System.out.println(x > y); // returns true, because 5 is higher than 3
+			
+
+	}
+
+}
